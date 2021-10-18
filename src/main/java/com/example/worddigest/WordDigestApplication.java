1c95d18151eb558ab5f77bff1059f8ff1b2ef54d
cailliaud <cailliaud.a@gmail.com>
2021-10-18 08:58:26 +0200

    [Word Digest] Setup project

---
+++ src/main/java/com/example/worddigest/WordDigestApplication.java
@@ -0,0 +1,12 @@
+package com.example.worddigest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordDigestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WordDigestApplication.class);
+  }
+}
---
+++ src/test/java/com/example/worddigest/WordDigestApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.worddigest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordDigestApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
